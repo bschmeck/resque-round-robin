d2ff85984e2d483c2ff1da63950515d6f7f9dd51
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2016-11-14 12:46:54 -0600

    Remove unused require

--- lib/resque-timed-round-robin.rb
+++ lib/resque-timed-round-robin.rb
@@ -1,7 +1,6 @@
 
 require 'resque'
 require 'resque/worker'
-require 'resque-dynamic-queues'
 require "resque/plugins/timed_round_robin/version"
 require "resque/plugins/timed_round_robin/timed_round_robin"
 
